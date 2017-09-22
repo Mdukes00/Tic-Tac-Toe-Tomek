c8e18f9fefb15f77e990a1d76b7d8c31bf6150f8
Michael d <mdukes00@live.com>
2017-09-22 09:31:39 +0200

    added git ignore file

---
+++ TicTacToeTomek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeTomek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
